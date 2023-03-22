c3627372104528638966dd469149711d767a1631
Damenjo Sitepu <damenjos@thebigrichgroup.com>
2023-03-22 13:52:31 +0700

    First Build: Basic of class implementation

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * Documentation
+ * 
+ * @method static sayHello(string $name)
+ */
+class Hello {
+    /**
+     * Say hello to anybody inside this worlds
+     * 
+     * @param string
+     * @return string
+     */
+    public static function sayHello(string $name = 'Damenjo'): string
+    {
+        return "Hello {$name}";
+    }
+}
+
+$app = new Hello;
+echo $app::sayHello('Menjo');
